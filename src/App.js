536cd101803bd1d27dff8b4e71d3059be34ded2a
CHINTAPALLI NAGA SYAMALA <syamala_csd@srkrec.edu.in>
2025-03-12 23:03:58 +0530

    client side

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Home"; // Import the Home component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+      <Home /> {/* Render the Home component */}
+    </div>
+  );
+}
+
+export default App;
